53f1ed9f91e8b01b2a4e0ff8acfd59b78f4486cf
Pablo <lomeliplablo1@gmail.com>
2024-04-02 16:36:44 -0700

    mesh generator now uses chunks

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -7,24 +7,27 @@ using Vector3 = UnityEngine.Vector3;
 using Vector2 = UnityEngine.Vector2;
 
 public static class MeshGenerator {
-    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve)
+    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail)
     {
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
         float topLeftX = (width - 1)/ -2f;
         float topleftZ = (height - 1)/ 2f;
 
-        MeshData meshData = new MeshData(width, height);
+        int meshSimplificationIncrement = (levelOfDetail == 0)? 1 : levelOfDetail * 2;
+        int verticesPerLine = (width-1)/meshSimplificationIncrement+1;
+
+        MeshData meshData = new MeshData(verticesPerLine, verticesPerLine);
         int vertexIndex = 0;
 
-        for(int y = 0; y < height; y++){
-            for(int x = 0; x < width; x++){
+        for(int y = 0; y < height; y += meshSimplificationIncrement){
+            for(int x = 0; x < width; x += meshSimplificationIncrement){
                 meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightCurve.Evaluate(heightMap[x,y]) * heightMultiplier, topleftZ - y);
                 meshData.uvs[vertexIndex] = new Vector2(x/(float)width, y/(float)height);
 
                 if(x < width - 1 && y < height - 1){
-                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width); // left triangle (imagine square)
-                    meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1); // right triagle
+                    meshData.AddTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine); // left triangle (imagine square)
+                    meshData.AddTriangle(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1); // right triagle
                 }
 
                 vertexIndex++;
